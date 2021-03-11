02b870d8c30e487851df2a85c44676b67b1a3d47
Carlos H Kruger <carloshkruger@gmail.com>
2021-03-11 17:05:09 -0300

    refactor(server): refactor AppError

--- packages/server/src/core/errors/AppError.ts
+++ packages/server/src/core/errors/AppError.ts
@@ -1,11 +1,14 @@
 class AppError extends Error {
   public readonly statusCode: number
+  public readonly customError = true
 
   constructor(message: string, statusCode: number = 400) {
     super(message)
 
     this.name = AppError.name
     this.statusCode = statusCode
+
+    Object.setPrototypeOf(this, AppError.prototype)
   }
 }
 
