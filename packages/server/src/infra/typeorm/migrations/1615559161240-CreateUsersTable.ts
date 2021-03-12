82d854b9933701276ea6ce4666b5a804197f84e1
Carlos H Kruger <carloshkruger@gmail.com>
2021-03-12 11:56:01 -0300

    chore(server): add typeorm package and create migrations

---
+++ packages/server/src/infra/typeorm/migrations/1615559161240-CreateUsersTable.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUsersTable1615559161240 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar'
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'cpf',
+            type: 'varchar',
+            length: '11'
+          },
+          {
+            name: 'is_admin',
+            type: 'boolean'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
---
+++ packages/server/src/infra/typeorm/migrations/1615559387312-CreateDeliveriesTable.ts
@@ -0,0 +1,83 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateDeliveriesTable1615559387312 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'deliveries',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'delivery_man_id',
+            type: 'uuid',
+            isNullable: true
+          },
+          {
+            name: 'product_name',
+            type: 'varchar'
+          },
+          {
+            name: 'recipient_name',
+            type: 'varchar'
+          },
+          {
+            name: 'address',
+            type: 'varchar'
+          },
+          {
+            name: 'neighborhood',
+            type: 'varchar'
+          },
+          {
+            name: 'city',
+            type: 'varchar'
+          },
+          {
+            name: 'state',
+            type: 'varchar'
+          },
+          {
+            name: 'postal_code',
+            type: 'varchar'
+          },
+          {
+            name: 'number',
+            type: 'integer',
+            isNullable: true
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'users',
+            columnNames: ['delivery_man_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('deliveries')
+  }
+}
