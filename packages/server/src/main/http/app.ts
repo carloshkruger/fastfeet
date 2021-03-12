0199c214d536554b0573d42ed58729929d814a36
Carlos H Kruger <carloshkruger@gmail.com>
2021-03-12 16:32:16 -0300

    chore(server): add express json parser and error handler

--- packages/server/src/main/http/app.ts
+++ packages/server/src/main/http/app.ts
@@ -1,5 +1,16 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import { router } from './routes/routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(router)
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    return response.status(500).json({
+      error: 'Internal server error'
+    })
+  }
+)
 
 export default app
